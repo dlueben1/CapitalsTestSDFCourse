8063b74b7a2c68d0d1b8b4629cad759d21f7bf21
Douglas Lueben <dlueben1@umbc.edu>
2021-02-26 00:08:10 +0000

    Added Georgia's Capital

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ capitals = {
     "West Virginia" : "Charleston"
 }
 
-### Add your code here: ###
-
 capitals["Virginia"] = "Richmond"
 capitals["Maryland"] = "Annapolis"
+capitals["Georgia"] = "Atlanta"
 
 capitals.pop("California")
 
 capitals["Delaware"] = "Dover"
-
-### Add your code above: ###
 
 for key, value in capitals.items():
   print(f"State: {key}, Capital: {value}")
